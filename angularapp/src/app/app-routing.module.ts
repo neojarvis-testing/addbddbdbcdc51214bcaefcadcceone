25132114cfb089023ba51057100e6a9347e07571
neojarvis <techadmin@iamneo.ai>
2025-04-10 07:07:23 +0000

    Auto-save-commit-10/04/2025--12:37:18

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddCustomerFormComponent } from './add-customer-form/add-customer-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/viewProduct', pathMatch: 'full' }, // Default route to view menu items
+  { path: 'addNewCustomer', component: AddCustomerFormComponent }, // Add Menu Item
+  { path: 'viewCustomer', component: Cus }, // View Menu Items
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
